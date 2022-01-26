4ac9c4dc939968245dc4f8e9fa297fb20955d167
JustKike <jaircervantes11@gmail.com>
2022-01-25 22:38:48 -0800

    Agregue NavBar

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,35 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+
+    <b-navbar toggleable="md" type="dark" variant="danger">
+      <b-container>
+        <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+        <b-navbar-brand href="#">The ComicCave</b-navbar-brand>
+
+        <b-collapse is-nav id="nav-collapse">
+          <b-navbar-nav>
+            <b-nav-item :to="{ name: 'Home' }" exact>Home</b-nav-item>
+            <b-nav-item :to="{ name: 'About' }">About</b-nav-item>
+            <b-nav-item :to="{ name: 'Grid' }">Grid</b-nav-item>
+            <b-nav-item :to="{ name: 'Cards' }">Cards</b-nav-item>
+            <b-nav-item :to="{ name: 'Form' }">Form</b-nav-item>
+          </b-navbar-nav>
+
+          <b-navbar-nav class="ml-auto">
+            <b-nav-form>
+              <b-form-input size="sm" class="mr-sm-2" placeholder="Search"></b-form-input>
+              <b-button size="sm" class="my-2 my-sm-0" type="submit">Search</b-button>
+            </b-nav-form>
+          </b-navbar-nav>
+          
+
+        </b-collapse>
+      </b-container>
+    </b-navbar>
+
+    <b-container>
+      <router-view />
+    </b-container>
+
   </div>
 </template>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
