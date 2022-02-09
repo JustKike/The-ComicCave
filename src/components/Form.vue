ae61aa73c3a467e3c06df19a65b576bf1899720e
slayer10484 <jose.ubietadiaz@cesunbc.edu.mx>
2022-02-09 02:43:35 -0800

    se agrego formulario de contacto

---
+++ src/components/Form.vue
@@ -0,0 +1,63 @@
+<template>
+    <div class="mt-5">
+<h1>Ponte en contacto con nosotros</h1>
+    
+    <form>
+    <b-col md="6">
+        <b-row>
+            <medium>Nombre</medium>
+        <b-form-input name="nombre" type="text"  v-model="texto" :state="comprobar" placeholder="Escribe tu nombre"></b-form-input>
+        <small>Al menos 3 caracteres</small>
+        </b-row>
+        <p></p>
+        <b-row>
+        <medium>Correo</medium>
+        <b-form-input type="email"  placeholder="Escribe tu correo"></b-form-input> 
+        </b-row>
+        <p></p>
+       
+        <b-row>
+        <medium>Comentario</medium>
+            <textarea class="form-control" placeholder="Escribe tu comentario"></textarea>
+        </b-row>
+
+        <p></p>
+        
+       <b-row>
+           <b-check>Acepto términos y condiciones</b-check>
+       </b-row>
+ 
+    </b-col>
+            
+    <b-col md="1">
+        <p></p>
+        <b-row>
+                <button class="btn btn-primary" type="submit">Enviar datos</button>
+        </b-row>
+    </b-col>
+    </form>
+
+    </div>
+</template>
+
+
+
+<script>
+
+export default {
+    name: 'Form',
+    data(){
+        return{
+            texto: ''
+        }
+    },
+    computed:{
+        comprobar(){
+            return this.texto.length >2 ? true:false
+        },
+        enable(){
+            return this.texto.length=false ? enable : disable
+        }
+    }
+}
+</script>
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,57 @@
+import Footer from '@/components/Footer.vue';
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1 class="mt-5">FUNDADORES DE COMICCAVE-HISTORIA</h1> 
+   
+   
+   <!-- Carta de Jair-->
+   <b-row class="mt-4" id="tarjetaJair">
+   <b-col cols="12" md="4">
+   <h4>Jair Cervantes</h4>
+    <b-card dir="Kike.jpg" img-alt="Jair Cervantes" img-left class="mb -3">
+      <b-card-text>
+        Jair Cervantes fue el miembro original del proyecto, es originario de Tijuana y la persona que aportó con la idea, tiene experiencia en el desarrollo web frontend que es lo que mejor se le da debido a su gran creatividad al momento de diseñar interfaces.
+      </b-card-text>
+    </b-card>
+   </b-col>
+   </b-row>
+
+
+   <b-row class="mt-4" id="tarjetaJose">
+   <b-col cols="12" md="4">
+   <h4>Jose Ubieta</h4>
+    <b-card dir="jose.png" img-alt="Jose Ubieta" img-right class="mb -3">
+      <b-card-text>
+        José Ubieta es uno de los miembros fundadores, impulsado por la idea de Jair de hacer una página web donde varias personas pudieran disfrutar de sus cómics e interactuar.
+      </b-card-text>
+    </b-card>
+   </b-col>
+   </b-row> 
+
+
+
+<Footer></Footer>
+
   </div>
+
 </template>
+
+<style scoped>
+
+.mt-5{
+position: absolute;
+left:400px;
+}
+
+#tarjetaJair{
+  position: absolute;
+  top: 600px;
+}
+
+#tarjetaJose{
+position: absolute;
+top: 900px;
+}
+
+</style>
+
