6ed27d33865dab06e8bd62f2ea107fa04ecc25fb
slayer10484 <jose.ubietadiaz@cesunbc.edu.mx>
2022-02-13 17:36:55 -0800

    se mejoró footer y se añadieron pruebas a página de lanzamientos

--- src/views/Releases.vue
+++ src/views/Releases.vue
@@ -1,6 +1,42 @@
 <template>
   <div class="releases">
-    <h1>This is a release page</h1>
+    <h1>Lanzamientos más recientes</h1>
+  
+  <b-row>
+    <b-col cols="12" md="6">
+       <b-card
+    title="Card Title"
+    img-src="https://picsum.photos/600/300/?image=25"
+    img-alt="Image"
+    img-top
+    tag="article"
+    style="max-width: 20rem;"
+    class="mb-2">
+    <b-card-text>
+      Some quick example text to build on the card title and make up the bulk of the card's content.
+    </b-card-text>
+
+    <b-button href="#" variant="primary">Go somewhere</b-button>
+  </b-card>
+    </b-col>
+        <b-col cols="12" md="6">
+       <b-card
+    title="Card Title"
+    img-src="https://picsum.photos/600/300/?image=25"
+    img-alt="Image"
+    img-top
+    tag="article"
+    style="max-width: 20rem;"
+    class="mb-2">
+    <b-card-text>
+      Some quick example text to build on the card title and make up the bulk of the card's content.
+    </b-card-text>
+
+    <b-button href="#" variant="primary">Go somewhere</b-button>
+  </b-card>
+    </b-col>
+  </b-row>
+  
   </div>
 </template>
 <script>
